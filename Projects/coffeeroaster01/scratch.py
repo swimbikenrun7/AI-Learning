8c8f22cb0efc89f6dbc681b401726f561eee23bc
Josh George <josh.george7@gmail.com>
2026-08-25 22:01:26 -0400

    Mission 0.9 complete

---
+++ Projects/coffeeroaster01/scratch.py
@@ -0,0 +1,16 @@
+current_roast = {
+        "date": "08/25/2026",
+        "bean": "Misty Valley",
+        "green_weight": 205,
+        "finished_weight": 181,
+        "roast_time": "08:15",
+        "first_crack": "06:45"
+}
+
+#print(current_roast["date"])
+#print(current_roast["bean"])
+#print(current_roast["green_weight"])
+#print(current_roast["finished_weight"])
+#print(current_roast["roast_time"])
+#print("first crack: ", current_roast["first_crack"])
+print(current_roast)
--- Projects/coffeeroaster02/main.py
+++ Projects/coffeeroaster02/main.py
@@ -6,7 +6,7 @@ def validate_date(date_str):
     date_format = "%m/%d/%Y"
     while True:
         try:
-            return datetime.strptime(date_str, date_format)
+            return datetime.strptime(date_str, date_format).date()
         except ValueError:
             print("Invalid date format. Please enter a date in the format MM/DD/YYYY.")
             date_str = input("Enter the date (MM/DD/YYYY): ")
@@ -29,28 +29,39 @@ def validate_time_input(prompt, min_val=None, max_val=None):
             hours, minutes = map(int, time_str.split(':'))
             total_minutes = hours * 60 + minutes
             if (min_val is None or total_minutes >= min_val) and (max_val is None or total_minutes <= max_val):
-                return time_str
+                return total_minutes
             else:
                 print(f"Invalid input. Please enter a time in MM:SS format between {min_val} and {max_val}.")
         else:
             print("Invalid input. Please enter a time in MM:SS format.")
 
+def create_roast_session(date, green_weight, finished_weight, total_roast_time, time_of_first_crack):
+    return {
+        'date': date,
+        'green_weight': green_weight,
+        'finished_weight': finished_weight,
+        'total_roast_time_seconds': total_roast_time,
+        'time_of_first_crack_seconds': time_of_first_crack
+    }
+
 def main():
     date = validate_date(input("Enter the date (MM/DD/YYYY): "))
     green_weight = validate_numeric_input("Enter the green weight (in grams): ", 100, 300)
     finished_weight = validate_numeric_input("Enter the finished weight (in grams): ", 100, green_weight)
-    total_roast_time = validate_time_input("Enter the total roast time (MM:SS): ", min_val=240, max_val=1200)  # 4 minutes to 20 minutes in minutes
-    time_of_first_crack = validate_time_input(f"Enter the time of first crack (MM:SS), must be less than {total_roast_time}: ", max_val=int(total_roast_time.split(':')[0]) * 60 + int(total_roast_time.split(':')[1]))
-    weight_loss_percentage = calculate_weight_loss(green_weight, finished_weight)
-    development_seconds = calculate_development_time(total_roast_time, time_of_first_crack)
+    total_roast_time = validate_time_input("Enter the total roast time (MM:SS): ", min_val=240, max_val=1200)  # 4 minutes to 20 minutes
+    time_of_first_crack = validate_time_input(f"Enter the time of first crack (MM:SS), must be less than {total_roast_time}: ", max_val = total_roast_time)
+    weight_loss_percentage = calculate_weight_loss(roast_session['green_weight'], roast_session['finished_weight'])
+    development_seconds = calculate_development_time(roast_session['total_roast_time_seconds'], roast_session['time_of_first_crack'])
+    development_time_str = f"{development_seconds // 60:02}:{(development_seconds % 60) // 60:02}"
     roast_classification = get_roast_classification(weight_loss_percentage)
+    roast_session = create_roast_session(date, green_weight, finished_weight, total_roast_time, time_of_first_crack)
 
     print("\nRoast Session Details:")
-    print(f"Date: {date.strftime('%m/%d/%Y')}")
-    print(f"Green Weight: {green_weight} grams")
-    print(f"Finished Weight: {finished_weight} grams")
+    print(f"Date: {roast_session['date']}")
+    print(f"Green Weight: {roast_session['green_weight']} grams")
+    print(f"Finished Weight: {roast_session['finished_weight']} grams")
     print(f"Weight Loss Percentage: {weight_loss_percentage:.2f}%")
-    print(f"Development Time: {format_development_time(development_seconds)}")
+    print(f"Development Time: {development_time_str}")
     print(f"Roast Classification: {roast_classification}")
 
 if __name__ == "__main__":
